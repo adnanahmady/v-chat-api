3b8947a749b9a6f26ed27507e92db9d917f4c683
adnanahmady <adnan.ahmady1394@gmail.com>
2019-09-26 22:14:54 +0330

    Add Soft delete to users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,12 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'username' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => bcrypt('secret'),
+        'access' => $faker->randomElement(['public', 'protected']),
+        'activated_at' => $faker->randomElement([now(), NULL]),
         'remember_token' => Str::random(10),
+        'deleted_at' => $faker->randomElement([now(), NULL]),
     ];
 });
